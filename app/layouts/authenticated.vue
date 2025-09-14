ba6430118eb59c49221bed0faba376b3591c7274
Favour  Emeka <favorodera@gmail.com>
2025-09-14 19:42:39 +0100

    Update UI configurations and enhance chat layout
    - Changed primary UI color from green to black in app.config.ts.
    - Added new CSS variables for primary UI color in index.css.
    - Enhanced authenticated layout with additional buttons for creating and searching chats.
    - Updated chat page layout to include a dashboard panel with a prompt for user interaction.

--- app/layouts/authenticated.vue
+++ app/layouts/authenticated.vue
@@ -8,30 +8,77 @@
       toggle-side="right"
       :ui="{
         root: 'transition-all duration-300 ease-in-out bg-elevated/25',
+        content: 'max-w-xs',
       }"
     >
 
-      <template #header="{ collapsed }">
+      <template #header>
 
-    <UDashboardSidebarCollapse></UDashboardSidebarCollapse>
+        <UDashboardSidebarCollapse />
 
       </template>
 
       <template #default="{ collapsed }">
+
+        <div class="flex flex-col gap-1.5">
+
+          <UButton
+            icon="lucide:edit"
+            :label="collapsed ? undefined : 'New Chat'"
+            variant="ghost"
+            class="w-full"
+          />
+
+          <UButton
+            icon="lucide:search"
+            :label="collapsed ? undefined : 'Search Chats'"
+            variant="ghost"
+            class="w-full"
+            @click="isCommandPaletteOpen = true"
+          />
+
+        </div>
 
         <UNavigationMenu
           :collapsed="collapsed"
           orientation="vertical"
           tooltip
           popover
+          :ui="{ link: 'overflow-hidden' }"
           :unmount-on-hide="false"
-        />
-        
+        >
+          <template #chat-trailing>
+            <div class="-mr-1.25 flex translate-x-full transition-transform group-hover:translate-x-0">
+              <UButton
+                icon="lucide:trash-2"
+                color="neutral"
+                variant="ghost"
+                size="xs"
+                class="p-0.5 text-muted hover:bg-accented/50 hover:text-primary focus-visible:bg-accented/50"
+                tabindex="-1"
+              />
+            </div>
+          </template>
+        </UNavigationMenu>
 
       </template>
 
-
     </UDashboardSidebar>
+
+    <UDashboardSearch
+      v-model:open="isCommandPaletteOpen"
+      :color-mode="false"
+      placeholder="Search chats..."
+      :groups="[{
+        id: 'links',
+        items: [
+          {
+            label: 'New chat',
+            icon: 'lucide:square-pen',
+          },
+        ],
+      }]"
+    />
 
     <slot />
 
@@ -41,4 +88,5 @@
 
 
 <script setup lang="ts">
+const isCommandPaletteOpen = ref(false)
 </script>
